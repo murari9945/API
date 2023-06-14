9a892f0e8a3c32bf89788ca5f89655d9df9eb3fc
murari9945 <kolli.pavanmurari@gmail.com>
2023-06-14 14:08:02 +0530

    api

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies,setMovies]=useState([]);
+ async function movieFetchHandler(){
+  const response= await fetch('https://swapi.dev/api/films')
+  const data =await response.json()
+  const getMovies=data.results.map((movieData)=>{
+    return{
+      id:movieData.episode_id,
+      title:movieData.title,
+      openingText:movieData.opening_crawl,
+      
+    }
+  });
+  setMovies(getMovies);
+
+
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={movieFetchHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
